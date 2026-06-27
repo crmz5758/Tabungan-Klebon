862cbd155a263a7a9511c35aaa45982f8e6b5169
crmz5758 <dimuscustom565@gmail.com>
2026-06-28 00:18:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,79 @@
-fetch("data.json")
-    .then(res => res.json())
-    .then(data => {
+let semuaAnggota = [];
 
-        const anggota = data.anggota;
+const totalAnggota = document.getElementById("totalAnggota");
+const totalSaldo = document.getElementById("totalSaldo");
+const tableBody = document.getElementById("tableBody");
+const search = document.getElementById("search");
+const tanggal = document.getElementById("tanggal");
 
-        document.getElementById("totalAnggota").textContent = anggota.length;
+function rupiah(angka) {
+    return "Rp" + angka.toLocaleString("id-ID");
+}
 
-        const total = anggota.reduce((sum, item) => sum + item.saldo, 0);
+function renderTable(data) {
 
-        document.getElementById("totalSaldo").textContent =
-            "Rp" + total.toLocaleString("id-ID");
+    tableBody.innerHTML = "";
+
+    if (data.length === 0) {
+
+        tableBody.innerHTML = `
+            <tr>
+                <td colspan="3" style="text-align:center;padding:30px;">
+                    Data tidak ditemukan
+                </td>
+            </tr>
+        `;
+
+        return;
+    }
+
+    data.forEach((item, index) => {
+
+        tableBody.innerHTML += `
+            <tr>
+                <td>${index + 1}</td>
+                <td>${item.nama}</td>
+                <td>${rupiah(item.saldo)}</td>
+            </tr>
+        `;
 
     });
+
+}
+
+fetch("data.json")
+.then(res => res.json())
+.then(data => {
+
+    semuaAnggota = data.anggota;
+
+    tanggal.textContent =
+        "Update terakhir : " + data.updateTerakhir;
+
+    totalAnggota.textContent =
+        semuaAnggota.length;
+
+    const total = semuaAnggota.reduce((sum, item) => {
+
+        return sum + item.saldo;
+
+    }, 0);
+
+    totalSaldo.textContent =
+        rupiah(total);
+
+    renderTable(semuaAnggota);
+
+});
+
+search.addEventListener("keyup", function () {
+
+    const keyword = this.value.toLowerCase();
+
+    const hasil = semuaAnggota.filter(item =>
+        item.nama.toLowerCase().includes(keyword)
+    );
+
+    renderTable(hasil);
+
+});
