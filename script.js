987c77d5edd14bc3233d79755fdf256cf1523a1b
crmz5758 <dimuscustom565@gmail.com>
2026-06-28 01:03:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+fetch('data.json')
+  .then(res => res.json())
+  .then(data => {
+    const tbody = document.getElementById('table-body');
+    const totalEl = document.getElementById('grand-total');
 
+    let grandTotal = 0;
+
+    data.data.forEach(item => {
+      grandTotal += item.total;
+
+      const row = `
+        <tr>
+          <td>${item.no}</td>
+          <td>${item.nama}</td>
+          <td>Rp ${item.total.toLocaleString('id-ID')}</td>
+        </tr>
+      `;
+
+      tbody.innerHTML += row;
+    });
+
+    totalEl.innerText = `Rp ${grandTotal.toLocaleString('id-ID')}`;
+  });
