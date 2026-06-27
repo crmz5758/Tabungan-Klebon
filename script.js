ca1941141cfc88039ab7372a14d2908a8ae0a116
crmz5758 <dimuscustom565@gmail.com>
2026-06-28 00:08:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-document.getElementById("totalAnggota").textContent = "0";
-document.getElementById("totalSaldo").textContent = "Rp0";
+fetch("data.json")
+    .then(res => res.json())
+    .then(data => {
+
+        const anggota = data.anggota;
+
+        document.getElementById("totalAnggota").textContent = anggota.length;
+
+        const total = anggota.reduce((sum, item) => sum + item.saldo, 0);
+
+        document.getElementById("totalSaldo").textContent =
+            "Rp" + total.toLocaleString("id-ID");
+
+    });
