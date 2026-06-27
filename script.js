2dc74ab140dd9a4830b2447aad66bf4119282683
crmz5758 <dimuscustom565@gmail.com>
2026-06-28 01:37:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,133 +1,86 @@
 let semuaData = [];
 
-
-// AMBIL DATA JSON
-
 fetch("data.json")
-
 .then(response => response.json())
-
 .then(data => {
-
 
     semuaData = data.anggota;
 
+    updateWaktu();
 
-    // update terakhir
-
-    document.getElementById("update").innerHTML = data.update;
-
-
-
-    // total orang
+    setInterval(updateWaktu,1000);
 
     document.getElementById("jumlah").innerHTML =
     semuaData.length + " Orang";
 
-
-
-    // total saldo
-
     hitungSaldo(semuaData);
 
-
-
-    // tampil tabel
-
     tampilData(semuaData);
-
 
 });
 
 
+function updateWaktu(){
+
+    let waktu = new Date();
+
+    document.getElementById("update").innerHTML =
+    waktu.toLocaleString("id-ID");
+
+}
 
 
-
-// HITUNG SALDO
 
 function hitungSaldo(data){
-
 
     let total = data.reduce(
         (jumlah,item)=> jumlah + item.nominal,
         0
     );
 
-
     document.getElementById("saldo").innerHTML =
     "Rp " + total.toLocaleString("id-ID");
-
 
 }
 
 
 
-
-
-// TAMPIL DATA
-
 function tampilData(data){
-
 
     let tabel = document.getElementById("data");
 
-
-    tabel.innerHTML = "";
-
+    tabel.innerHTML="";
 
 
     data.forEach((item,index)=>{
 
-
         tabel.innerHTML += `
 
         <tr>
-
             <td>${index+1}</td>
-
             <td>${item.nama}</td>
-
-            <td>
-            Rp ${item.nominal.toLocaleString("id-ID")}
-            </td>
-
+            <td>Rp ${item.nominal.toLocaleString("id-ID")}</td>
         </tr>
 
         `;
 
-
     });
-
 
 }
 
 
 
-
-
-// SEARCH NAMA
-
-document
-.getElementById("search")
+document.getElementById("search")
 .addEventListener("keyup",function(){
-
 
     let keyword = this.value.toLowerCase();
 
 
-
     let hasil = semuaData.filter(item =>
-
-        item.nama
-        .toLowerCase()
-        .includes(keyword)
-
+        item.nama.toLowerCase().includes(keyword)
     );
 
 
-
     tampilData(hasil);
-
-
 
 });
